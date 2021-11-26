0e1452e5ceed0a0292b118960d6ae3f1ee62eb19
Ola <olanasrat@gmail.com>
2021-11-26 23:03:43 +0100

    API integration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 
 function displayTemperature(response) {
-    console,console.log(response.data.main.temp);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    let cityElement = document.querySelector("#City");
+    cityElement.innerHTML = response.data.name;
+    let descriptionElement = document.querySelector("#descrition");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    let humidityElement = document.querySelector("#Humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+    let windElement = document.querySelector("#Wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+
 }
 
 
