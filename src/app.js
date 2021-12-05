4b940bc90883d15ba1f82c38b3d4776724467512
Ola <olanasrat@gmail.com>
2021-12-05 17:34:07 +0100

    Java forecast

--- src/app.js
+++ src/app.js
@@ -16,7 +16,25 @@ function displayTemperature(response) {
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
     celsiusTemperature = response.data.main.temp;
 
+
+    getForecast(response.data.coord);
+
 }
+
+
+function getForecast(coordinates) {
+    let apiKey = "a969311cfcbb4a83dfad2cf7478397f9";
+    let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    console.log(apiURL);
+        axios.get(apiURL).then(displayForecast);
+
+    
+}
+
+
+
+
+
 
 function formatDate(timestamp) {
     let date = new Date(timestamp);
@@ -44,26 +62,27 @@ let apiKey = "a969311cfcbb4a83dfad2cf7478397f9";
 
 }
 
-function displayForecast() {
+function displayForecast(response) {
     let forecastElement = document.querySelector("#forecast");
-    forecastElement.innerHTML = `
-     <div class="row">
+
+    let forecastHTML = `<div class="row">`;
+    let days = ["Mon", "Tue", "Wed", "Thu"];
+    days.forEach(function (day) {
+        forecastHTML = forecastHTML + `
         <div class="col">
-            <div class="weather-forecast-date"> Friday</div>
-              <br>
+            <div class="weather-forecast-date"> ${day}</div>
             ☀️
-            <br>
             <div class="weather-forecast.temperature">
                 <span class="weather-forecast-temperature-max"> 26°</span>
                 <span class="weather-forecast-temperature-min">20°</span>
            </div>
-        </div>
-        </div>
-        </div>
-`;
+        </div>`;
 
+    })
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
 }
-
+    
 function handleSubmit(event) {
     event.preventDefault();
     let cityinputElement = document.querySelector("#city-input");
